0abaeee881cd4f8a08636131ad51139a1e9a2e56
AndyTRY <andy.yuen@dashhudson.com>
2024-06-08 20:20:19 -0400

    stuff

--- src/services/gemeni/base.ts
+++ src/services/gemeni/base.ts
@@ -1,11 +1,16 @@
 import { GoogleGenerativeAI } from "@google/generative-ai";
 const genAI = new GoogleGenerativeAI(import.meta.env.VITE_APP_GEMENI_API_KEY ?? '');
 const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
+const chat = model.startChat({
+  history: [],
+  generationConfig: {
+    maxOutputTokens: 100,
+  },
+});
 
-
-async function prompt(promptText: string): Promise<string> {
+async function prompt(msg: string): Promise<string> {
     try {
-      const result = await model.generateContent(promptText);
+      const result = await chat.sendMessage(msg);
       const response = await result.response;
       const text = response.text();
       return text;
