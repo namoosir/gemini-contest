769636940785e7d10699b1cbcc492585259b4073
tahbib osman <to2001628@gmail.com>
2024-07-05 00:32:44 -0400

    wip: create interview page
    Co-authored-by: Nazmus Saqeeb <namoosir@users.noreply.github.com>

--- src/components/Steps.tsx
+++ src/components/Steps.tsx
@@ -1,16 +1,51 @@
-import React from "react";
+import { mdiCheck, mdiCircleOutline } from "@mdi/js";
+import Icon from "@mdi/react";
+
+import { cn } from "@/lib/utils";
+
+interface Step {
+  label: string;
+  completed: boolean;
+}
 
 interface Props {
-  currentStep: 1 | 2 | 3
+  currentStep: number;
 }
-export default function Steps(props: Props): React.FC {
+
+export default function Steps(props: Props) {
+  const steps: Step[] = [
+    { label: "Job", completed: props.currentStep >= 1 },
+    { label: "Resume", completed: props.currentStep >= 2 },
+    { label: "Preferences", completed: props.currentStep >= 3 },
+  ];
 
   return (
-    <div>
-      <div>
-        <div className=""></div>
-
-      </div>
+    <div className="flex flex-row justify-center items-center">
+      {steps.map((step, index) => (
+        <div key={index} className="flex flex-row justify-center items-center">
+          <div className="flex flex-row justify-center items-center gap-1">
+            <Icon
+              className={cn(
+                "h-4 w-4 text-muted-foreground",
+                (index > 0 ? steps[index - 1].completed : true) ? "text-primary" : ""
+              )}
+              path={step.completed ? mdiCheck : mdiCircleOutline}
+            />
+          </div>
+          <p className={cn("ml-1 text-muted-foreground", step.completed ? "text-primary" : "")}>
+            {step.label}
+          </p>
+          {index < steps.length - 1 && (
+            <div
+              className={cn(
+                "w-10 h-1 rounded-lg",
+                step.completed ? "bg-primary" : "bg-muted-foreground",
+                "mx-2"
+              )}
+            />
+          )}
+        </div>
+      ))}
     </div>
-  )
+  );
 }
--- src/components/pages/Interview.tsx
+++ src/components/pages/Interview.tsx
@@ -12,16 +12,22 @@ import {
 } from "@/components/ui/card";
 import { Button } from "../ui/button";
 import { Textarea } from "../ui/textarea";
+import ComboBox from "../ComboBox";
+import { Input } from "../ui/input";
+import { Label } from "../ui/label";
+import Steps from "../Steps";
+
+type Page = 0 | 1 | 2;
 
 const Interview: React.FC = () => {
-  const [currentPage, setCurrentPage] = useState(0);
+  const [currentPage, setCurrentPage] = useState<Page>(0);
 
   const handleNextPage = () => {
-    setCurrentPage((prevPage) => prevPage + 1);
+    setCurrentPage((prevPage: Page): Page => (prevPage + 1) as Page);
   };
 
   const handlePreviousPage = () => {
-    setCurrentPage((prevPage) => prevPage - 1);
+    setCurrentPage((prevPage: Page): Page => (prevPage - 1) as Page);
   };
 
   const renderPage = () => {
@@ -39,13 +45,13 @@ const Interview: React.FC = () => {
 
   return (
     <div className="h-full flex flex-col py-12 items-center">
-      <div className="max-w-[700px]">
+      <div className="max-w-[700px] min-w-[700px]">
         <h3 className="text-2xl font-semibold leading-none tracking-tight mb-4">
           Interview
         </h3>
         <Card>
           <CardHeader>
-            <p>Page {currentPage + 1} of 3</p>
+            <Steps currentStep={currentPage} />
           </CardHeader>
           {renderPage()}
           <CardFooter>
@@ -55,7 +61,7 @@ const Interview: React.FC = () => {
                 variant="secondary"
                 onClick={handlePreviousPage}
               >
-                <Icon className='h4 w-4 mr-2' path={mdiArrowLeft} />
+                <Icon className="h4 w-4 mr-2" path={mdiArrowLeft} />
                 Previous
               </Button>
 
@@ -66,20 +72,20 @@ const Interview: React.FC = () => {
                   onClick={handleNextPage}
                 >
                   Next
-                  <Icon className='h4 w-4 ml-2' path={mdiArrowRight} />
+                  <Icon className="h4 w-4 ml-2" path={mdiArrowRight} />
                 </Button>
               )}
 
-              {currentPage === 2 &&
+              {currentPage === 2 && (
                 <Button
                   className="ml-auto"
                   variant="default"
                   onClick={handleNextPage}
                 >
                   Start
-                  <Icon className='h4 w-4 ml-2' path={mdiCheck} />
+                  <Icon className="h4 w-4 ml-2" path={mdiCheck} />
                 </Button>
-              }
+              )}
             </div>
           </CardFooter>
         </Card>
@@ -90,11 +96,12 @@ const Interview: React.FC = () => {
 
 const JobDescriptionCard: React.FC = () => {
   return (
-    <>
+    <div className="w-full">
       <CardHeader>
         <CardTitle>Job Description</CardTitle>
         <p className="text-base text-muted-foreground">
-          Please enter the job description you would like to simulate an interview for.
+          Please enter the job description you would like to simulate an
+          interview for.
         </p>
       </CardHeader>
       <CardContent>
@@ -103,29 +110,45 @@ const JobDescriptionCard: React.FC = () => {
           This will be used to generate relevant questions for your interview.
         </p>
       </CardContent>
-    </>
+    </div>
   );
 };
 
 const ResumeCard: React.FC = () => {
+  const tempItems = [
+    { value: "react", label: "React" },
+    { value: "vue", label: "Vue" },
+    { value: "angular", label: "Angular" },
+  ];
+
   return (
-    <>
+    <div className="w-full">
       <CardHeader>
         <CardTitle>Resume</CardTitle>
       </CardHeader>
-      <CardContent>{/* Add your resume content here */}</CardContent>
-    </>
+      <CardContent>
+        <ComboBox items={tempItems} name={"Resume"} />
+        <div className="grid w-full max-w-sm items-center gap-1.5 ">
+          <Label htmlFor="resume">Upload Resume</Label>
+          <Input
+            id="resume"
+            type="file"
+            className="file:text-muted-foreground"
+          />
+        </div>
+      </CardContent>
+    </div>
   );
 };
 
 const InterviewCard: React.FC = () => {
   return (
-    <>
+    <div className="w-full">
       <CardHeader>
         <CardTitle>Interview</CardTitle>
       </CardHeader>
       <CardContent>{/* Add your interview content here */}</CardContent>
-    </>
+    </div>
   );
 };
 
