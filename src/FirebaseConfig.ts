3231a286e375dcda51836d1c1b3bc9bec43cbe6b
Yadu-M <ymadhu@torontomu.ca>
2024-07-13 22:55:53 -0400

    added callable function (for tts)

--- src/FirebaseConfig.ts
+++ src/FirebaseConfig.ts
@@ -4,6 +4,7 @@ import { getFirestore, connectFirestoreEmulator } from "firebase/firestore";
 import { getStorage, connectStorageEmulator } from "firebase/storage";
 import { getAuth } from "firebase/auth";
 import { GoogleAuthProvider } from "firebase/auth";
+import { getFunctions, connectFunctionsEmulator } from "firebase/functions";
 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -22,6 +23,7 @@ export const app = initializeApp(firebaseConfig);
 export const analytics = getAnalytics(app);
 export const db = getFirestore(app);
 export const storage = getStorage(app);
+export const functions = getFunctions(app);
 
 export const auth = getAuth(app);
 export const provider = new GoogleAuthProvider();
@@ -30,3 +32,4 @@ auth.languageCode = "it";
 // Connects app with firestore and storage emulators.
 connectFirestoreEmulator(db, "127.0.0.1", 8080);
 connectStorageEmulator(storage, "127.0.0.1", 9199);
+connectFunctionsEmulator(functions, "127.0.0.1", 5001);
--- src/services/voice/TTS.ts
+++ src/services/voice/TTS.ts
@@ -1,29 +1,31 @@
-export const fetchAudioBuffer = async (sentence: string): Promise<{word: string, buffer: Uint8Array}[]> => {
-  try {
-    const response = await fetch('http://127.0.0.1:5001/gemini-contest/us-central1/api/audio/tts', {
-      method: "POST",
-      body: JSON.stringify({ "text": sentence, "model": "" })
-    });
-    return await response.json() as Promise<{word: string, buffer: Uint8Array}[]>
-  } catch (error) {
-    alert('Something went wrong while trying to fetch, maybe turn on cloud function or fix backend')
-    throw new Error(`${error as string}`)
-  }
-}
+import { Functions, httpsCallable } from "firebase/functions";
 
-// export const playAudio = (arrayBuffer: ArrayBuffer, audioCtx: AudioContext) => {
-//   return new Promise<void>(resolve => {
-//     audioCtx.decodeAudioData(arrayBuffer, (buffer) => {
-//       const source = audioCtx.createBufferSource()
-//       source.buffer = buffer
-//       source.connect(audioCtx.destination)
-//       source.start(0)
-//       source.onended = () => {
-//         resolve()
-//       }
-//     }, error => {
-//       console.error('Error decoding audio data:', error)
-//       throw new Error(`Something went wrong while trying to play audio ${error}`)
-//     })
-//   })
-// }
+export const fetchAudioBuffer = async (sentence: string) => {
+  try {
+    const response = await fetch(
+      "http://127.0.0.1:5001/gemini-contest/us-central1/api/audio/tts",
+      {
+        method: "POST",
+        body: JSON.stringify({ text: sentence, model: "" }),
+      }
+    );
+    return (await response.json()) as Promise<
+      { word: string; buffer: Uint8Array }[]
+    >;
+  } catch (error) {
+    throw new Error(error as string);
+  }
+};
+
+export const fetchAudioBufferV2 = async (
+  sentence: string,
+  functions: Functions
+) => {
+  try {
+    const getTTS = httpsCallable(functions, "tts");
+    const result = await getTTS({ text: sentence, model: "" });
+    return result.data as Promise<{ word: string; buffer: Uint8Array }[]>;
+  } catch (error) {
+    throw new Error(error as string);
+  }
+};
