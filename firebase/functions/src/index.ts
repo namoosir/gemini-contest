404f9a3472c0f635c44f03de131ee49eaa903ad2
Yadu-M <ymadhu@torontomu.ca>
2024-07-20 21:31:02 -0400

    tts optimize

--- firebase/functions/src/index.ts
+++ firebase/functions/src/index.ts
@@ -1,20 +1,20 @@
-import admin = require("firebase-admin")
-import express = require("express")
-import { https } from "firebase-functions/v2"
-import { getTTS } from "./deepgram"
+import admin = require("firebase-admin");
+import express = require("express");
+import { https } from "firebase-functions/v2";
+import { getTTS } from "./deepgram";
 
-admin.initializeApp()
-const app = express()
+admin.initializeApp();
+const app = express();
 
 app.post("/audio/tts", async (req, res) => {
   try {
-    const { text } = JSON.parse(req.body)
-    res.status(200).send(await getTTS(text))
+    const { text } = req.body;
+    res.status(200).send(await getTTS(text));
   } catch (error) {
-    res.status(500).send("Cudnt get tts rip")
+    res.status(500).send("Cudnt get tts rip");
   }
-})
+});
 
 // TODO: add endpoint for STT
 
-exports.api = https.onRequest(app)
+exports.api = https.onRequest(app);
--- src/services/voice/TTS.ts
+++ src/services/voice/TTS.ts
@@ -1,29 +1,18 @@
-export const fetchAudioBuffer = async (sentence: string): Promise<{word: string, buffer: Uint8Array}[]> => {
-  try {
-    const response = await fetch('http://127.0.0.1:5001/gemini-contest/us-central1/api/audio/tts', {
+export const fetchAudioBuffer = async (sentence: string) => {
+  const response = await fetch(
+    "http://127.0.0.1:5001/gemini-contest/us-central1/api/audio/tts",
+    {
       method: "POST",
-      body: JSON.stringify({ "text": sentence, "model": "" })
-    });
-    return await response.json() as Promise<{word: string, buffer: Uint8Array}[]>
-  } catch (error) {
-    alert('Something went wrong while trying to fetch, maybe turn on cloud function or fix backend')
-    throw new Error(`${error as string}`)
-  }
-}
+      body: JSON.stringify({ text: sentence, model: "" }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    }
+  );
 
-// export const playAudio = (arrayBuffer: ArrayBuffer, audioCtx: AudioContext) => {
-//   return new Promise<void>(resolve => {
-//     audioCtx.decodeAudioData(arrayBuffer, (buffer) => {
-//       const source = audioCtx.createBufferSource()
-//       source.buffer = buffer
-//       source.connect(audioCtx.destination)
-//       source.start(0)
-//       source.onended = () => {
-//         resolve()
-//       }
-//     }, error => {
-//       console.error('Error decoding audio data:', error)
-//       throw new Error(`Something went wrong while trying to play audio ${error}`)
-//     })
-//   })
-// }
+  if (!response.ok) {
+    const errorText = await response.text();
+    throw new Error(`Backend error: ${response.status} ${errorText}`);
+  }
+  return await response.blob();
+};
